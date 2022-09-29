3e402b3bc6b6672dfe91e2aac59ea9a9f8af1842
ViolettaFilimonova <84204983+ViolettaFilimonova@users.noreply.github.com>
2022-09-29 08:20:55 +0300

    process.evn

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const colors = require('colors')
-console.log(colors.green('Hello'))
+// console.log(colors.green('Hello'))
+console.log(process.argv)
+
+function isNum(str){
+    // const num = JSON.parse(str)
+    console.log(typeof str[2])
+}
+
+
+console.log(process.env[2])
